35977ba49f8e1239e0d0968b14cfc02090c5dc1f
vivi m <vivianamaldes@gmail.com>
2021-04-13 17:10:50 -0500

    adds hazards websocket function

--- misty-py-wrapper/misty.py
+++ misty-py-wrapper/misty.py
@@ -1,8 +1,8 @@
-from .api_wrappers import ApiWrapperMixin
-from .asset import AssetMixin
-from .system import SystemMixin
-from .movement import Head, BothArms, DrivingMixin
-from .navigation import NavigationMixin
+from api_wrappers import ApiWrapperMixin
+from asset import AssetMixin
+from system import SystemMixin
+from movement import Head, BothArms, DrivingMixin
+from navigation import NavigationMixin
 from typing import List
 from io import BytesIO
 import gtts
@@ -40,6 +40,37 @@ class Robot(NavigationMixin, DrivingMixin, SystemMixin, AssetMixin, ApiWrapperMi
             conditions = [('sensorPosition', '!=', sensor) \
                           for sensor in exclude_sensors]
         self.add_websocket('TouchSensor', event_name, conditions=conditions, **kwargs)
+
+    def add_hazard_notification(self, event_name: str,
+                                hazard_type: List[str] = None, **kwargs):
+        """Add a WebSocket connection to receive hazard notification event messages.
+
+        Misty sends event messages each time the hazard system detects a change to a
+        hazard state. You can subscribe to these events to be
+        notified when Misty enters or exits a specific hazard state.
+        """
+
+        hazards = ['bumpSensorsHazardState', 'criticalInternalError', 'driveStopped'
+                   'timeOfFlightSensorsHazardState', 'excessiveSpeedHazard']
+        if not hazard_type:
+            conditions = None
+        # if only one type 
+        elif isinstance(hazard_type, str):
+            if hazard_type in hazards:
+                conditions = [('Hazard', '==', hazard_type)]
+            else:
+                raise ValueError(f'Invalid hazard type {hazard_type}')
+        # if more than one type
+        else:
+            exclude_hazards = hazards.copy()
+            for hazard in hazard_type:
+                if not hazard in hazards:
+                    raise ValueError(f'Invalid hazard type {hazard}')
+                else:
+                    exclude_hazards.remove(hazard)
+            conditions = [('Hazard', '!=', hazard) \
+                          for hazard in exclude_hazards]
+        self.add_websocket('HazardNotification', event_name, conditions=conditions, **kwargs)
 
     def text_to_speech(self, msg: str, lang: str = 'es',
                        file_name: str = 'temp.mp3',
